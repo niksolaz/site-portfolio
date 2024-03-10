7e7be9ee4bcf7473e1b9df3ca1115335f2fff7a2
Nicola Solazzo <solazzo.nicola@gmail.com>
2024-03-10 21:36:55 +0100

    upload new image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,45 +6,43 @@ const listCard = [
   {
     title: "Sviluppo",
     text: "Sviluppo di applicazioni web e mobile",
-    price: 500
+    price: 500,
   },
   {
     title: "Design",
     text: "Design di interfacce utente e user experience",
-    price: 300
+    price: 300,
   },
   {
     title: "Consulenza",
     text: "Consulenza e formazione",
-    price: 100
-  }
-]
+    price: 100,
+  },
+];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-24">
       <Navbar />
-      <section className="text-center py-40">
+      <section className="py-40 text-center">
         <Image
-          src="/astronaut-hero-img.webp"
+          src="/astrox.webp"
           alt="Astronaut Hero Image"
           width={380}
           height={37}
           priority
         />
-        <h2 className="block text-8xl bg-gradient-to-r from-yellow-400 to-yellow-800 text-transparent bg-clip-text py-5">
+        <h2 className="block bg-gradient-to-r from-yellow-400 to-yellow-800 bg-clip-text py-5 text-8xl text-transparent">
           NIXO
         </h2>
-        <p className="text-3xl  text-white">
-          Trasforma le tue idee in realtà.
-        </p>
+        <p className="text-3xl  text-white">Trasforma le tue idee in realtà.</p>
       </section>
-      <section className="mx-auto py-40 flex flex-wrap items-center justify-center gap-y-4 lg:grid lg:grid-cols-3 lg:gap-3">
-      {
-        listCard.map((card) => {
-          return <Card title={card.title} text={card.text} price={card.price} />
-        })
-      }
+      <section className="mx-auto flex flex-wrap items-center justify-center gap-y-4 py-40 lg:grid lg:grid-cols-3 lg:gap-3">
+        {listCard.map((card) => {
+          return (
+            <Card title={card.title} text={card.text} price={card.price} />
+          );
+        })}
       </section>
     </main>
   );
