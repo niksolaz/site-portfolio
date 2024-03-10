e5ec9efe92baaecfb6009bd634821ea04e8a2b9a
Nicola Solazzo <solazzo.nicola@gmail.com>
2024-03-10 21:57:11 +0100

    fixed new image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
+import About from "../components/About";
 
 const listCard = [
   {
@@ -18,31 +19,50 @@ const listCard = [
     text: "Consulenza e formazione",
     price: 100,
   },
+  {
+    title: "Api",
+    text: "Sviluppo di backend e API",
+    price: 500,
+  },
+  {
+    title: "Gestione progetti",
+    text: "Design di interfacce utente e user experience",
+    price: 300,
+  },
+  {
+    title: "Caffè",
+    text: "Consulenza e formazione",
+    price: 100,
+  }
 ];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-24">
       <Navbar />
-      <section className="py-40 text-center">
+      <section className="py-20 text-center">
         <Image
           src="/astrox.webp"
           alt="Astronaut Hero Image"
           width={380}
           height={37}
           priority
+          className="rounded-full shadow-yellow-400 shadow-lg mx-auto mb-10"
         />
         <h2 className="block bg-gradient-to-r from-yellow-400 to-yellow-800 bg-clip-text py-5 text-8xl text-transparent">
           NIXO
         </h2>
         <p className="text-3xl  text-white">Trasforma le tue idee in realtà.</p>
       </section>
-      <section className="mx-auto flex flex-wrap items-center justify-center gap-y-4 py-40 lg:grid lg:grid-cols-3 lg:gap-3">
+      <section className="mx-auto flex flex-wrap items-center justify-center gap-y-4 py-10 lg:grid lg:grid-cols-3 lg:gap-3">
         {listCard.map((card) => {
           return (
             <Card title={card.title} text={card.text} price={card.price} />
           );
         })}
+      </section>
+      <section className="mx-auto flex flex-wrap items-center justify-center gap-y-4 py-10 lg:grid lg:grid-cols-3 lg:gap-3">
+        <About />
       </section>
     </main>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 z-10 max-w-5xl w-full flex items-center justify-between font-mono text-sm px-10 bg-black sm:bg-transparent">
+    <div className="fixed top-0 z-10 max-w-5xl w-full flex items-center justify-between font-mono text-sm px-10 bg-black">
         <Link href="/" className="py-8 backdrop-blur-2xl text-yellow-400">
         NIXO ><span className="animate-pulse">_</span>
         </Link>
