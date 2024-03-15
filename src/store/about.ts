8d7d48a7679c7dcf4feeef6440416976160fe959
Nicola Solazzo <solazzo.nicola@gmail.com>
2024-03-15 21:04:44 +0100

    Update about.ts

--- src/store/about.ts
+++ src/store/about.ts
@@ -5,6 +5,8 @@ export const about: About = `
   Pensi di avere un progetto che potrebbe essere interessante? 
   Mi chiamo Nicola e sono un programmatore con esperienza pluriennale.
   Il mio obiettivo è quello di trasformare le tue idee in realtà.
-  Sono specializzato in sviluppo web e mobile.
+  Sono specializzato in sviluppo web e Desktop.
+  I miei focus tecnologici sono Vue.js/Nuxt.js e React.js/Next.js per il frontend e Node.js per il backend.
+  Lato desktop mi occupo di sviluppo con Electron.js. Mentre per task di automazione mi avvalgo di Python.
   `
 
