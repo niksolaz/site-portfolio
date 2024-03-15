df45d59c1dc41f15881fa8a5fceb4d6a5515df82
Nicola Solazzo <solazzo.nicola@gmail.com>
2024-03-15 14:59:49 +0100

    add env file

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,114 @@
-import Image from "next/image";
+'use client'
 import Navbar from "../../components/Navbar";
+import Alert from '../../components/Alert'
+import React, { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import useAlert from '../../hooks/useAlert'
 
 export default function Contact() {
+  const formRef = useRef(null)
+  const [form, setForm] = useState({ name: '', email: '', message: '' })
+  const [isLoading, setIsLoading] = useState(false)
+  
+  const { alert, showAlert, hideAlert } = useAlert()
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const serviceId = process.env.EMAILJS_SERVICE_ID;
+    const templateId = process.env.EMAILJS_TEMPLATE_ID;
+    const publicKey = process.env.EMAILJS_PUBLIC_KEY;
+
+    console.log(serviceId, templateId, publicKey)
+    
+    /*
+    emailjs.send(
+      serviceId!,
+      templateId!,
+      {
+        from_name: form.name,
+        to_name: 'Nicola',
+        from_email: form.email,
+        to_email: 'solazzo.nicola@gmail.com',
+        message: form.message
+      },
+      publicKey
+    ).then(() => {
+      setIsLoading(false);
+      showAlert({ show: true, text: 'Message sent successfully', type: 'success' } as { show: boolean, text: string, type?: string });
+      setTimeout(() => {
+        hideAlert();
+        setForm({ name: '', email: '', message: '' });
+      }, 3000)
+    }).catch(err => {
+      setIsLoading(false);
+      console.error(err)
+      showAlert({ show: true, text: 'An error occurred, please try again later', type: 'danger' } as { show: boolean, text: string, type?: string });
+    })
+    */
+  };
+  
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Navbar />
-            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-                Contact
-            </div>
+         <Navbar />
+         {alert.show && <Alert {...alert} />}
+         <div className="flex-1 min-w-[50%] flex flex-col">
+            <h1 className="head-text">Get in touch</h1>
+            <form 
+              className="w-full flex flex-col gap-7 mt-14"
+              onSubmit={handleSubmit}
+            >
+              <label className="ext-black-50 font-semibold">
+                Your Name
+                <input 
+                  type="text" 
+                  name="name"
+                  className="input"
+                  placeholder="Mario Rossi"
+                  required
+                  value={form.name}
+                  onChange={handleChange}
+                />
+              </label>
+              <label className="ext-black-50 font-semibold">
+                Your Email
+                <input 
+                  type="email" 
+                  name="email"
+                  className="input"
+                  placeholder="email@example.com"
+                  required
+                  value={form.email}
+                  onChange={handleChange}
+                />
+              </label>
+              <label className="ext-black-50 font-semibold">
+                Your Message
+                <textarea 
+                  name="message"
+                  rows={4}
+                  className="textarea"
+                  placeholder="let me know how I can help you"
+                  required
+                  value={form.message}
+                  onChange={handleChange}
+                />
+              </label>
+              <button
+                type="submit"
+                className="btn"
+                disabled={isLoading}
+              >
+                {isLoading ? 'Sending...' : 'Send Message'}
+              </button>
+            </form>
+          </div>
         </main>
     )
 }
---
+++ src/components/Alert.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Alert = ({type, text, show}) => {
+  return (
+    <div className="absolute top-10 left-0 right-0 flex justify-center items-center">
+        <div 
+            className={`${type === 'danger' ? 'bg-red-800' : 'bg-blue-800'} p-2 text-indigo-100 leading-none z-10 rounded-full flex lg:inline-flex items-center space-x-2`} 
+            role="alert"
+        >
+            <p
+                className={`${type === 'danger' ? 'bg-red-500' : 'bg-blue-800'} uppercase font-semibold text-xs sm:text-sm px-2 py-1 rounded-full text-center`}
+            >
+                {type === 'danger' ? 'Failed' : 'Success'}
+            </p>
+            <p className="mr-2 text-left">{text}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Alert
